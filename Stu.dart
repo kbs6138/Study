b7dfc95464d83bcb38bf03c87f02c4c65d4829e4
kbs6138 <kbs6138@naver.com>
2023-12-12 23:10:35 +0900

    collection if, for

---
+++ Stu.dart
@@ -0,0 +1,10 @@
+void main() {
+  var oldfriends = ['nico', 'jara'];
+  var newFriends = [
+    'kevin',
+    'polo',
+    'col',
+    for (var myfriends in oldfriends) "🥰 $myfriends"
+  ];
+  print(newFriends);
+}
